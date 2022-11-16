aaa207745ac06041a7e788fbd74df5292dbca798
huynhdthanhtuan <tuan40655@gmail.com>
2022-11-17 00:35:13 +0700

    Setup DB Main's models. Next is DB Crawl.

---
+++ models/DB_Main/index.js
@@ -0,0 +1,13 @@
+const UserModel = require("./User");
+const SharkModel = require("./Shark");
+const TokenModel = require("./Token");
+const TagModel = require("./Tag");
+const TransactionModel = require("./Transaction");
+
+module.exports = {
+    UserModel,
+    SharkModel,
+    TokenModel,
+    TagModel,
+    TransactionModel,
+};
